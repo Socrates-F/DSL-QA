b5b7b143637a1edd79c88a3c83b888c7dc385ada
Socrates-F <93358148+Socrates-F@users.noreply.github.com>
2025-12-13 18:50:37 -0300

    Update SeleniumGenerator.py

--- src/SeleniumGenerator.py
+++ src/SeleniumGenerator.py
@@ -1,12 +1,18 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+
 from TestScriptVisitor import TestScriptVisitor
 from TestScriptParser import TestScriptParser
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
 
 options = Options()
 options.add_argument("--headless=new")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--disable-gpu")
+options.add_argument("--window-size=1920,1080")
 
 driver = webdriver.Chrome(options=options)
 
